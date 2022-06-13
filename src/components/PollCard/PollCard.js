49803f91b3c3c0ddaee7aebd4561fbca02c1c0c6
gaubabysieuquay <dungnguyenduong27@gmail.com>
2022-06-13 17:00:21 +0700

    Update

--- src/components/PollCard/PollCard.js
+++ src/components/PollCard/PollCard.js
@@ -1,24 +1,41 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import "./PollCard.scss";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './PollCard.scss';
 
 class PollCard extends Component {
+  // isAnswered = () => {
+  //   const { question } = this.props;
+  //   const { author, id, optionOne, optionTwo } = question;
+  //   const tab = 'Unanswered';
+  //   const isAnswered =
+  //     optionOne.votes.includes(author) || optionTwo.votes.includes(author);
+  //   let poll = {};
+  //   if (tab === 'Unanswered' && !isAnswered) {
+  //     return (poll = question);
+  //   } else {
+  //     console.log('Answered');
+  //   }
+  // };
+
   render() {
     console.log(this.props);
+
+    const { question } = this.props;
+    const { authorAvatarUrl, authorName, id, optionOne, optionTwo } = question;
+
     return (
       <div className="card">
-        <div className="card-header">Someone asks:</div>
+        <div className="card-header">{authorName} asks:</div>
         <div className="card-content">
           <div className="poll">
             <div className="avatar">
-              <img
-                src="https://demoda.vn/wp-content/uploads/2022/01/avatar-doremon.jpg"
-                alt=""
-              />
+              <img src={authorAvatarUrl} alt={authorName} />
             </div>
             <div className="question">
               <p className="title">Would you rather</p>
-              <p>Question here ?</p>
+              <p>{optionOne.text}</p>
+              <p>OR</p>
+              <p>{optionTwo.text}</p>
               <button className="btn-outlined">View Poll</button>
             </div>
           </div>
@@ -28,13 +45,33 @@ class PollCard extends Component {
   }
 }
 
-const mapStateToProps = ({ authUser, user, questions }, { id }) => {
+const mapStateToProps = ({ authUser, users, questions }, { id }) => {
   const question = questions[id];
+  const author = question ? users[question.author] : null;
+  // const tab = 'Unanswered';
+  // const isAnswered =
+  //   question.optionOne.votes.includes(authUser) ||
+  //   question.optionTwo.votes.includes(authUser);
+
+  // let answeredPoll = {};
+  // let unansweredPoll = {};
+
+  // if (!isAnswered) {
+  //   answeredPoll = question;
+  // } else {
+  //   unansweredPoll = question;
+  // }
+
+  // const poll = tab === 'Unanswered' ? answeredPoll : unansweredPoll;
 
   return {
     authUser,
-    question: {},
+    question: {
+      ...question,
+      authorAvatarUrl: author.avatarURL,
+      authorName: author.name,
+    },
   };
 };
 
-export default connect()(PollCard);
+export default connect(mapStateToProps)(PollCard);
